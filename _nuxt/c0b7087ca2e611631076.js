f01f87d4961c51298bc3d8bcd7381df9d6412da6
gersom <gersomalaja@gmail.com>
2019-10-26 00:45:35 -0500

    complete peliculas id page
    complete peliculas id page

---
+++ _nuxt/c0b7087ca2e611631076.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{181:function(n,t,e){"use strict";e.r(t);var r={name:"Inicio"},c=e(18),component=Object(c.a)(r,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("nuxt-link",{attrs:{to:"/peliculas"}},[t("h1",[this._v("Ingresar")])])],1)}),[],!1,null,"64942a27",null);t.default=component.exports}}]);
