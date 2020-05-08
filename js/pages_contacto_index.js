8a65eb07ff2ab3a89e754b9bfaa674eaba7fdeec
Gersom Alaja <gersomalaja@gmail.com>
2020-05-08 14:27:02 -0500

    Proyect Elitland

---
+++ js/pages_contacto_index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{179:function(t,n,e){"use strict";e.r(n);var c={name:"Contact"},r=e(20),component=Object(r.a)(c,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("h1",[this._v("Contacto")])])}],!1,null,null,null);n.default=component.exports}}]);
